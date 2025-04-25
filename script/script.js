5cf5ab51c8835ab1f5dc92e07f5877ff26ef3424
terencengdev <terencejiahao@gmail.com>
2025-04-25 13:44:12 +0800

    Added Matter js demo playground

--- script/script.js
+++ script/script.js
@@ -1,11 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
+  const text = new SplitType("#main_heading");
+  const video = document.getElementById("videoTest");
   gsap.registerPlugin(ScrollTrigger);
+
+  gsap.set("#main_heading", { autoAlpha: 1 });
+  gsap.set(text.chars, { yPercent: 100 });
+
+  const textAnimate = gsap.to(text.chars, {
+    yPercent: 0,
+    duration: 1,
+    ease: "sine.out",
+    stagger: { from: "left", amount: 0.5 },
+  });
 
   let tl1 = gsap.timeline({
     scrollTrigger: {
       trigger: ".hero-section",
       start: "top top",
-      end: "3800",
+      end: "bottom",
+      pin: true,
+      scrub: 3,
+    },
+  });
+
+  let tl2 = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".video-section",
+      start: "top",
+      end: "bottom",
       pin: true,
       scrub: 3,
     },
@@ -16,8 +38,8 @@ document.addEventListener("DOMContentLoaded", () => {
       opacity: 0,
     })
     .to(".logo", {
-      scale: 1,
-      y: 5,
+      scale: 0.5,
+      y: 1,
     })
     .fromTo(
       ".hero-image .clip",
@@ -36,19 +58,5 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .fromTo(".hero-bottom .description  ", { opacity: 0 }, { opacity: 1 });
 
-  //   gsap.to(".logo", {
-  //     scale: 1,
-  //     y: 5,
-  //     delay: 2,
-  //   });
-
-  //   gsap.to(".hero-section .description", {
-  //     opacity: 0,
-  //     scrollTrigger: {
-  //       trigger: ".hero-section",
-  //       start: "top  10%",
-  //       pin: "#hero-section .pin-wrap",
-  //       scrub: 1,
-  //     },
-  //   });
+  tl2.to(video, { currentTime: 20 });
 });
